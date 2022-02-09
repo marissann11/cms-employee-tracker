cb476477c49f72ae969b627f163ba847a3595738
marissann11 <wilhite.marissa@gmail.com>
2022-02-09 01:41:26 -0500

    created files and wrote sql schema

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+drop database if exists employee;
+create database employee;
+use employee;
+
+drop table if exists departments;
+drop table if exists roles;
+drop table if exists employees;
+
+create table departments (
+    id integer auto_increment primary key,
+    department_name varchar(30) not null
+);
+
+create table roles (
+    id integer auto_increment primary key,
+    title varchar(30) not null,
+    salary decimal(10,2),
+    dept_id integer,
+    constraint fk_dept foreign key (dept_id) references departments(id) on delete set null
+);
+
+create table employees (
+    id integer auto_increment primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id integer,
+    constraint fk_role foreign key (role_id) references roles(id) on delete set null
+);
+
+-- add manager id after role id in employee table
